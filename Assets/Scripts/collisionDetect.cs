f11f898666642c27dc17973572af19a854dd714d
demansn <demansn@gmail.com>
2014-03-27 13:40:48 +0200

    Create decals controller, fixed collision detect

--- Assets/Scripts/collisionDetect.cs
+++ Assets/Scripts/collisionDetect.cs
@@ -7,53 +7,14 @@ using Edelweiss.DecalSystem;
 namespace Edelweiss.DecalSystem.Example {
 	
 	public class collisionDetect : MonoBehaviour {
-		private Vector3 viewPos;
-		private Vector3 changePos;
-		private GameObject collisionGameObject;
-		private bool hasCreateDecal = false;
-		public GameObject particlePrefab;
-		private GameObject createdParticle;
-		private GameConttroller gameController;
 
-		// The prefab with the ready to use DS_Decals. The material and uv rectangles are set up.
-		[SerializeField] private DS_Decals m_DecalsPrefab = null;
-		
-		// The reference to the instantiated prefab's DS_Decals instance.
-		private DS_Decals m_DecalsInstance;
-		private Matrix4x4 m_WorldToDecalsMatrix;
-		
-		// We keep track of the projectors that we are creating at runtime. If we have more than
-		// the maximum number of projectors, we remove the oldest ones.
-		private List <DecalProjector> m_DecalProjectors = new List <DecalProjector> ();
-		[SerializeField] private int m_MaximumNumberOfProjectors = 50;
-		
-		// We need a decals mesh and a cutter to create the decal projectors at runtime.
-		private DecalsMesh m_DecalsMesh;
-		private DecalsMeshCutter m_DecalsMeshCutter;
-		
-		// We can't place the projector exactly at the place where the raycast hits a collider. Instead,
-		// it needs to be moved back, such that the projection looks believable. This value indicates how
-		// many units the projector needs to be moved back.
-		[SerializeField] private float m_DecalProjectorOffset = 0.5f;
-		
-		// The size, culling angle and mesh offset for the decal projectors for the bullets we are about to
-		// create at runtime.
-		[SerializeField] private Vector3 m_DecalProjectorScale = new Vector3 (2f, 2f, 2f);
-		[SerializeField] private float m_CullingAngle = 80.0f;
-		[SerializeField] private float m_MeshOffset = 2.0f;
-		
-		// We iterate through all the defined uv rectangles. This one inicates which index we are using at
-		// the moment.
-		private int m_UVRectangleIndex = 0;
-		
-		// Move on to the next uv rectangle index. We call this method after each projection. In practice
-		// you certainly don't do that, but pick an appropriate one depending on the surface that was hit.
-		private void NextUVRectangleIndex () {
-			m_UVRectangleIndex = m_UVRectangleIndex + 1;
-			if (m_UVRectangleIndex >= m_DecalsInstance.CurrentUvRectangles.Length) {
-				m_UVRectangleIndex = 0;
-			}
-		}
+		public int decalsId = 0;
+		public GameObject particlePrefab;
+
+		private Vector3 contactPoint;
+		private bool hasCreateDecal = false;
+		private GameObject collisionGameObject;
+		private GameConttroller gameController;
 		
 		private void Start () {
 
@@ -61,135 +22,57 @@ namespace Edelweiss.DecalSystem.Example {
 
 		}
 		
-		private void Update () {
-			
+		private void Update () {			
 			
 			if (hasCreateDecal) {
 
-				Ray l_Ray = Camera.main.ViewportPointToRay (viewPos);
+				Vector3 viewPoint = Camera.main.WorldToViewportPoint(contactPoint);
+				Ray l_Ray = Camera.main.ViewportPointToRay (viewPoint);
 				RaycastHit l_RaycastHit;
 
-
 				if (Physics.Raycast (l_Ray, out l_RaycastHit, Mathf.Infinity)) {
+
+					DecalsController[] dControllers = collisionGameObject.GetComponents<DecalsController>();
 		
-					// Collider hit.
-				
-	
-					// Make sures there are not too many projectors.
-					if (m_DecalProjectors.Count >= m_MaximumNumberOfProjectors) {
-						
-						// If there are more than the maximum number of projectors, we delete
-						// the oldest one.
-						if(m_DecalProjectors.Count != 0){
-							DecalProjector l_DecalProjector = m_DecalProjectors [0];
-							m_DecalProjectors.RemoveAt (0);
-							m_DecalsMesh.RemoveProjector (l_DecalProjector);
+					foreach(DecalsController decalsController in dControllers){
+						if(decalsId == decalsController.decalsId){
+							decalsController.AddDecalProjector(l_Ray, l_RaycastHit);
 						}
 					}
-					
-					// Calculate the position and rotation for the new decal projector.
-					Vector3 l_ProjectorPosition = changePos - (m_DecalProjectorOffset * l_Ray.direction.normalized);
-					Quaternion l_ProjectorRotation = ProjectorRotationUtility.ProjectorRotation (- l_RaycastHit.normal, Vector3.up);
-					
-					// Randomize the rotation.
-					Quaternion l_RandomRotation = Quaternion.Euler (0.0f, Random.Range (0.0f, 360.0f), 0.0f);
-					l_ProjectorRotation = l_ProjectorRotation * l_RandomRotation;
-					
-				
-					
-					// We hit a collider. Next we have to find the mesh that belongs to the collider.
-					// That step depends on how you set up your mesh filters and collider relative to
-					// each other in the game objects. It is important to have a consistent way in order
-					// to have a simpler implementation.
-					
-					MeshCollider l_MeshCollider = l_RaycastHit.collider.GetComponent <MeshCollider> ();
-					MeshFilter l_MeshFilter = l_RaycastHit.collider.GetComponent <MeshFilter> ();
-					
-					if (l_MeshCollider != null || l_MeshFilter != null) {
-						Mesh l_Mesh = null;
-						if (l_MeshCollider != null) {
-							
-							// Mesh collider was hit. Just use the mesh data from that one.
-							l_Mesh = l_MeshCollider.sharedMesh;
-						} else if (l_MeshFilter != null) {
-							
-							// Otherwise take the data from the shared mesh.
-							l_Mesh = l_MeshFilter.sharedMesh;
-						}
-						
-						if (l_Mesh != null) {
-							
-							// Create the decal projector.
-							DecalProjector l_DecalProjector = new DecalProjector (l_ProjectorPosition, l_ProjectorRotation, m_DecalProjectorScale, m_CullingAngle, m_MeshOffset, m_UVRectangleIndex, m_UVRectangleIndex);
-							
-							// Add the projector to our list and the decals mesh, such that both are
-							// synchronized. All the mesh data that is now added to the decals mesh
-							// will belong to this projector.
-							m_DecalProjectors.Add (l_DecalProjector);
-							m_DecalsMesh.AddProjector (l_DecalProjector);
-							
-							// Get the required matrices.
-							Matrix4x4 l_WorldToMeshMatrix = l_RaycastHit.collider.renderer.transform.worldToLocalMatrix;
-							Matrix4x4 l_MeshToWorldMatrix = l_RaycastHit.collider.renderer.transform.localToWorldMatrix;
-							
-							// Add the mesh data to the decals mesh, cut and offset it.
-							m_DecalsMesh.Add (l_Mesh, l_WorldToMeshMatrix, l_MeshToWorldMatrix);						
-							m_DecalsMeshCutter.CutDecalsPlanes (m_DecalsMesh);
-							m_DecalsMesh.OffsetActiveProjectorVertices ();
-							
-							
-							// The changes are only present in the decals mesh at the moment. We have
-							// to pass them to the decals instance to visualize them.
-							m_DecalsInstance.UpdateDecalsMeshes (m_DecalsMesh);
-							
-							// For the next hit, use a new uv rectangle. Usually, you would select the uv rectangle
-							// based on the surface you have hit.
-							NextUVRectangleIndex ();
 
-							m_DecalsInstance.transform.parent = collisionGameObject.transform;
+					GameObject explode = Instantiate(particlePrefab, contactPoint, transform.rotation) as GameObject;
 
-							createdParticle = Instantiate(particlePrefab, changePos, transform.rotation) as GameObject;
-							Destroy(createdParticle, 5f);
 
-							gameObject.SetActive(false);
+					gameObject.SetActive(false);
 
-							hasCreateDecal = false;
-							
-						}
+					Invoke("ActivationGameObject", 0.5f);
+					Destroy(explode, 5);
 
-					}
+					hasCreateDecal = false;							
 				}
+
 			}
 		}
-		
+
+		private void ActivationGameObject(){
+			gameObject.SetActive(true);
+		}		
 	
 		public void OnCollisionEnter(Collision collision){
 			
 			if(!hasCreateDecal){
 
 				foreach (ContactPoint contact in collision.contacts) {
-					changePos = contact.point;								
+					contactPoint = contact.point;								
 				}
 
-				collisionGameObject = collision.gameObject;
-				viewPos = Camera.main.WorldToViewportPoint(collision.gameObject.transform.position);	
-
-				if(m_DecalsPrefab != null){
-					m_DecalsInstance = Instantiate (m_DecalsPrefab) as DS_Decals;
-				}
-				
-				if (m_DecalsInstance == null) {
-					Debug.LogError ("The decals prefab does not contain a DS_Decals instance!");
-				} else {
-					m_DecalsMesh = new DecalsMesh (m_DecalsInstance);
-					m_DecalsMeshCutter = new DecalsMeshCutter ();
-					m_WorldToDecalsMatrix = m_DecalsInstance.CachedTransform.worldToLocalMatrix;
-				}
+				collisionGameObject = collision.gameObject;				
+								
 
 				gameController.StartMoveBackward();		
 
 				hasCreateDecal = true;
-			}
+			}		
 		}
 	}
 }
