0fbea5f41608b58c9e01af7742821f654dcc829b
demansn <demansn@gmail.com>
2014-03-28 13:40:28 +0200

    Fixed trail scale

--- Assets/Scripts/TrailController.cs
+++ Assets/Scripts/TrailController.cs
@@ -6,6 +6,7 @@ public class TrailController : MonoBehaviour {
 	private GameObject targetObject;
 	private Vector3 previousPosition;
 	private Quaternion previousRotation;
+	private Vector3 previusScale;
 	private int stepCounter = 0;
 
 	public void SetProperty(GameObject target, float alpha){
@@ -13,14 +14,17 @@ public class TrailController : MonoBehaviour {
 		renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, alpha);
 		previousPosition = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, transform.position.z);
 		previousRotation = targetObject.transform.rotation;
+		previusScale = targetObject.transform.localScale;
 	}
 
 	void LateUpdate () {
 
-			transform.position = previousPosition;
-			transform.rotation = previousRotation;	
-			previousPosition = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, transform.position.z);
-			previousRotation = targetObject.transform.rotation;
+		transform.position = previousPosition;
+		transform.rotation = previousRotation;
+		transform.localScale = previusScale;
+		previousPosition = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, transform.position.z);
+		previousRotation = targetObject.transform.rotation;
+		previusScale = targetObject.transform.localScale;
 		
 	}
 }
